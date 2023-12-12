76a3f65df3b1d76a4800bb4d92c2a032af20b132
Mehdi Key <formk@kermed.fr>
2023-12-12 13:43:40 +0100

    Ajout fichier config

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,10 @@
 <?php
 
-$routes = array(
-    array('home','default','index')
-);
+// ['nom de la page','Nom du controller','nom de la methode','tableau d'arguments']
+
+$routes =  [
+    ['home','default','index']
+];
 
 
 
